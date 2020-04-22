6acef54d3beeb65fc271b6f25a19f73e76242252
anu <chipkar.anita09@gmail.com>
2020-04-22 13:19:36 +0530

    Initial Commit

---
+++ Covid19Tracker/app/src/main/java/com/covid19/covid19tracker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.covid19.covid19tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
